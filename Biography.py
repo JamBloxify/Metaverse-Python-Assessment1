576467e161f826bb53cb1ac441d3a0d42deb056c
JamBloxify <112192511+JamBloxify@users.noreply.github.com>
2024-09-25 16:33:18 +0400

    Add files via upload

---
+++ Biography.py
@@ -0,0 +1,8 @@
+# advanced
+thisdict = {"Name:": input("What is your first and second name? "),
+            "Hometown:": input("Where were you born? "),
+            "Age:": input("How old are you? ")
+    }
+
+for x, y in thisdict.items():
+  print(x, y) 
